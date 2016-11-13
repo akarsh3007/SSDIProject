e30012499e361a79101d198e2b42c48ec68b69d5
akarsh3007 <gupta.akarsh@gmail.com>
2016-11-13 17:00:39 -0500

    UI fixes and bug fixes

--- src/main/java/com/cuisineratingsystem/repositories/IDishRepository.java
+++ src/main/java/com/cuisineratingsystem/repositories/IDishRepository.java
@@ -1,5 +1,7 @@
 package com.cuisineratingsystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,5 +19,8 @@ public interface IDishRepository extends JpaRepository<Dish, Integer> {
 	@Transactional
 	@Query("UPDATE Dish d SET d.rating=:rating, d.no_of_raters=:no_of_raters WHERE d.dish_ID=:id")
 	 int updateRating(@Param("id") int id, @Param("rating") float rating, @Param("no_of_raters") int no_of_raters);
+	
+	@Query("select c from Dish c where c.dish_name like :dish_name")
+	 List<Dish> searchDish(@Param("dish_name")String dish_name);
 
 }
