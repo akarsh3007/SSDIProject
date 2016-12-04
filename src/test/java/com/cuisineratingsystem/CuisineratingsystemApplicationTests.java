b6508b66dae13aab511989634cda7adb38122061
akarsh3007 <gupta.akarsh@gmail.com>
2016-12-04 16:42:41 -0500

    Added Test application properties and made changes in Cuisine and Cuisie review Test Services to use test database

--- src/test/java/com/cuisineratingsystem/CuisineratingsystemApplicationTests.java
+++ src/test/java/com/cuisineratingsystem/CuisineratingsystemApplicationTests.java
@@ -2,15 +2,23 @@ package com.cuisineratingsystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableJpaRepositories("com.cuisineratingsystem.repositories")
+@EnableTransactionManagement
+@SpringBootApplication(scanBasePackages={"com.cuisineratingsystem.services","com.cuisineratingsystem.controllers","com.cuisineratingsystem.repositories","com.cuisineratingsystem.model"})
 public class CuisineratingsystemApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		
 	}
 
 }
